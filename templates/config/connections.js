a3c6011a3eb24cf760dfbd08e0cad6f24e09bf2a
John-Henry Liberty <johnhenry.liberty@gmail.com>
2016-03-09 18:47:26 -0600

    Adds sails-migrations package to deps.

--- templates/config/connections.js
+++ templates/config/connections.js
@@ -76,10 +76,12 @@ module.exports.connections = {
   ***************************************************************************/
   postgresql: {
     adapter: 'sails-postgresql',
-    host: 'YOUR_POSTGRES_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user: 'YOUR_POSTGRES_USER',
-    password: 'YOUR_POSTGRES_PASSWORD',
-    database: 'YOUR_POSTGRES_DB'
+    host: 'localhost',
+    port: 5432,
+    database: 'your_db_name_here',
+    user: '',
+    password: '',
+
   }
 
 
